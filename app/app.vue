005a655cd7d87047f38abadc46cd3ec1c960cd19
Ben Hong <ben@bencodezen.io>
2022-09-01 13:27:25 -0500

    refactor: move apps to their own components

--- app/app.vue
+++ app/app.vue
@@ -1,49 +1,26 @@
 <script setup>
-import { computed, ref } from 'vue'
-
-let photoGallery = ref([])
-
-const numberOfPhotos = computed(() => {
-  return photoGallery.value.length
-})
-
-const evenAlbums = computed(() => {
-  return photoGallery.value.filter(item => item.albumId % 2 === 0)
-})
-
-const oddAlbums = computed(() => {
-  return photoGallery.value.filter(item => !(item.albumId % 2 === 0))
-})
-
-function fetchPhotoGallery() {
-  fetch('https://jsonplaceholder.typicode.com/photos')
-    .then(response => response.json())
-    .then(json => {
-      photoGallery.value = json
-    })
-}
+import PhotoGallery from './components/PhotoGallery'
+import TodoViewer from './components/TodoViewer'
 </script>
 
 <template>
   <div class="container">
     <div class="section">
-      <h1>Photo Gallery</h1>
-      <button @click="fetchPhotoGallery">Fetch Data</button>
-      <p>
-        {{ numberOfPhotos }} photos ({{ oddAlbums.length }} odd albums |
-        {{ evenAlbums.length }} even albums)
-      </p>
-      <ul class="photo-gallery-list">
-        <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
-          <img :src="photo.thumbnailUrl" />
-        </li>
-      </ul>
+      <div class="columns">
+        <div class="column">
+          <TodoViewer />
+        </div>
+        <div class="column">
+          <PhotoGallery />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
 @import './node_modules/bulma/bulma.sass';
+@import './assets/styles/main.scss';
 
 .photo-gallery-list {
   display: grid;
