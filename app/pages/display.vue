a7bcb6d6a6fd8ed49f4ab256e998a72b560b1e1d
Ben Hong <ben@bencodezen.io>
2022-09-01 14:42:42 -0500

    feature: simplify display page

--- app/pages/display.vue
+++ app/pages/display.vue
@@ -3,25 +3,9 @@
 <template>
   <div class="container">
     <div class="section">
-      <div class="columns">
-        <div class="column">
-          <TodoViewer title="This is fun!">
-            <template v-slot:hero>
-              <h3>Whoa this worked!</h3>
-            </template>
-          </TodoViewer>
-        </div>
-        <div class="column">
-          <PhotoGallery />
-        </div>
-      </div>
+      <h1>Display</h1>
     </div>
   </div>
 </template>
 
-<style lang="scss">
-.photo-gallery-list {
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-}
-</style>
+<style lang="scss"></style>
