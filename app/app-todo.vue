3d455930236dbf36c4ea26c6968697582185c2c6
Ben Hong <ben@bencodezen.io>
2022-09-01 10:27:18 -0500

    feature: add todo list viewer example

---
+++ app/app-todo.vue
@@ -0,0 +1,43 @@
+<script>
+import { defineNuxtComponent } from '#app'
+
+export default defineNuxtComponent({
+  data: () => ({
+    todoList: []
+  }),
+  methods: {
+    fetchTodoList() {
+      fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(json => {
+          this.todoList = json
+        })
+    }
+  }
+})
+</script>
+
+<template>
+  <div>
+    <img src="/todo.jpg" alt="Todo photo by Glenn Casterns-Peters" />
+    <p>
+      Photo by
+      <a
+        href="https://unsplash.com/@glenncarstenspeters?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+        >Glenn Carstens-Peters</a
+      >
+      on
+      <a
+        href="https://unsplash.com/s/photos/todo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+        >Unsplash</a
+      >
+    </p>
+    <h1>Hello Frontend Masters!</h1>
+    <button @click="fetchTodoList">Fetch Data</button>
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" :checked="todo.completed" /> {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <h1>Photo Gallery</h1>
+</template>
+
+<style></style>
