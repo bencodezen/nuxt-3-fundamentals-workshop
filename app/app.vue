ab02295d5bcfd97285f31fa3e3b85dc2c3e2bf4d
Ben Hong <ben@bencodezen.io>
2022-09-01 11:21:54 -0500

    refactor: move todo to comp api

--- app/app.vue
+++ app/app.vue
@@ -1,55 +1,47 @@
 <script>
-import { defineNuxtComponent } from '#app'
+import { ref, computed } from 'vue'
 
-export default defineNuxtComponent({
-  data: () => ({
-    todoList: []
-  }),
-  computed: {
-    completedItems() {
-      return this.todoList.filter(item => item.completed)
-    },
-    remainingItems() {
-      return this.todoList.filter(item => !item.completed)
-    }
-  },
-  methods: {
-    fetchTodoList() {
-      fetch('https://jsonplaceholder.typicode.com/todos/')
-        .then(response => response.json())
-        .then(json => {
-          this.todoList = json
-        })
-    }
-  }
+const todoList = ref([])
+
+const completedItems = computed(() => {
+  return todoList.value.filter(item => item.completed)
 })
+
+const remainingItems = computed(() => {
+  return todoList.value.filter(item => !item.completed)
+})
+
+function fetchTodoList() {
+  fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then(response => response.json())
+    .then(json => {
+      todoList.value = json
+    })
+}
 </script>
 
 <template>
-  <div>
-    <img src="/todo.jpg" alt="Todo photo by Glenn Casterns-Peters" />
-    <p>
-      Photo by
-      <a
-        href="https://unsplash.com/@glenncarstenspeters?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-        >Glenn Carstens-Peters</a
-      >
-      on
-      <a
-        href="https://unsplash.com/s/photos/todo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-        >Unsplash</a
-      >
-    </p>
-    <h1>Hello Frontend Masters!</h1>
-    <button @click="fetchTodoList">Fetch Data</button>
-    <p>
-      {{ completedItems.length }} completed |
-      {{ remainingItems.length }} remaining
-    </p>
-    <ul>
-      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
-        <input type="checkbox" :checked="todo.completed" /> {{ todo.title }}
-      </li>
-    </ul>
-  </div>
+  <p>
+    Photo by
+    <a
+      href="https://unsplash.com/@glenncarstenspeters?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+      >Glenn Carstens-Peters</a
+    >
+    on
+    <a
+      href="https://unsplash.com/s/photos/todo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+      >Unsplash</a
+    >
+  </p>
+  <h1>Hello Frontend Masters!</h1>
+  <button @click="fetchTodoList">Fetch Data</button>
+  <p>
+    {{ completedItems.length }} completed |
+    {{ remainingItems.length }} remaining
+  </p>
+  <ul>
+    <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+      <input type="checkbox" :checked="todo.completed" /> {{ todo.title }}
+    </li>
+  </ul>
 </template>
