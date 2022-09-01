97917ec780a038adf390484fbbdbc09ddde38d4f
Ben Hong <ben@bencodezen.io>
2022-09-01 13:19:53 -0500

    feature: add styles to photo gallery

--- app/app.vue
+++ app/app.vue
@@ -25,17 +25,28 @@ function fetchPhotoGallery() {
 </script>
 
 <template>
-  <h1>Photo Gallery</h1>
-  <button @click="fetchPhotoGallery">Fetch Data</button>
-  <p>
-    {{ numberOfPhotos }} photos ({{ oddAlbums.length }} odd albums |
-    {{ evenAlbums.length }} even albums)
-  </p>
-  <ul>
-    <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
-      <img :src="photo.thumbnailUrl" />
-    </li>
-  </ul>
+  <div class="container">
+    <div class="section">
+      <h1>Photo Gallery</h1>
+      <button @click="fetchPhotoGallery">Fetch Data</button>
+      <p>
+        {{ numberOfPhotos }} photos ({{ oddAlbums.length }} odd albums |
+        {{ evenAlbums.length }} even albums)
+      </p>
+      <ul class="photo-gallery-list">
+        <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
+          <img :src="photo.thumbnailUrl" />
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style lang="scss">
+@import './node_modules/bulma/bulma.sass';
+
+.photo-gallery-list {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+}
+</style>
